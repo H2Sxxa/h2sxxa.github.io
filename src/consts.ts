813ab30bffe9c4f1d98a5c8838c4aa9d1d555b51
H2Sxxa <h2sxxa0w0@gmail.com>
2026-06-02 20:24:46 +0800

    feat: baselayout

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,31 @@
 
 export const SITE_TITLE = 'Blog name';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+
+// Home Page
+export const SITE_OWNER = {
+    name: "H2Sxxa",
+    bio: "Just Coding for Fun.",
+    avatar: "https://avatars.githubusercontent.com/u/88923783",
+    description: `
+Full-Stack Developer, Rustacean, Modder, Open-Source Builder...
+I think technology should make the world better, so I keep programming instead of 'coding'.
+    `,
+};
+
+export const FAB_LINKS = [
+    {
+        href: "/",
+        icon: "mdi:home",
+    },
+    {
+        href: "/blog",
+        icon: "mdi:blog",
+    },
+    {
+        href: "/about",
+        icon: "mdi:account",
+    },
+]
+
