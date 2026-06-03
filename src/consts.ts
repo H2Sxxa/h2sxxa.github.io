cf8906e6a83f2829fdc7b54dbd443b0e2d3dc64e
H2Sxxa <h2sxxa0w0@gmail.com>
2026-06-03 18:48:37 +0800

    fix: pnpm approve

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog name';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = `H2Sxxa's Blog`;
+export const SITE_DESCRIPTION = 'TODO: Add description';
 
 
 // Home Page
