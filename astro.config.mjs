273c4dae6a18244f7fc9585e26d4fa302d66fab4
H2Sxxa <h2sxxa0w0@gmail.com>
2026-06-05 18:50:59 +0800

    feat: enhance blog layout with new components and improve markdown processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
-
+import { rehypeHeadingIds, unified } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
@@ -18,6 +18,19 @@ export default defineConfig({
     })],
     image: {
         domains: ['avatars.githubusercontent.com'],
+    },
+    markdown: {
+        processor: unified({
+            rehypePlugins: [
+                rehypeHeadingIds,
+                [rehypeAutolinkHeadings, {
+                    behavior: "prepend", properties: {
+                        className: ["anchor"],
+                    }
+                }],
+            ],
+        }),
+
     },
     fonts: [
         { provider: fontProviders.fontsource(), name: 'Fira Sans', cssVariable: '--font-lato', fallbacks: ['sans-serif'] },
--- src/content/pages/about.md
+++
@@ -1,7 +0,0 @@
----
-title: About
-
-pubDate: 'June 03 2026'
----
-
-This is the about page. You can edit this page by modifying the `src/content/about.md` file.
