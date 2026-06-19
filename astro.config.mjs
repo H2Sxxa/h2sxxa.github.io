8d9168248c7038863a521fdb020352f1057e9771
H2Sxxa <h2sxxa0w0@gmail.com>
2026-06-19 14:01:19 +0800

    feat: update site URL, enhance BaseHead component, and remove unused assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://h2sxxa.github.io',
     integrations: [mdx(), sitemap(), icon({
         include: {
             mdi: ["*"],
--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = `H2Sxxa's Blog`;
-export const SITE_DESCRIPTION = 'TODO: Add description';
+export const SITE_DESCRIPTION = 'Just share something interesting.';
 
 
 // Home Page
